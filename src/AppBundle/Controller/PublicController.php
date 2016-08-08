a7197099fe67a921c7de8096a3f447b1541280b0
Juan <evovang@gmail.com>
2016-08-08 09:58:21 +0000

    update de base.html.twig y PublicControler.php

---
+++ src/AppBundle/Controller/PublicController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PublicController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homeAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('home/index.html.twig');
+    }
+}
