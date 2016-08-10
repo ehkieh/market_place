895873eb88428e20ab9216bd8ad0bea47f506c47
Ezequiel <toledano87@gmail.com>
2016-08-10 10:10:19 +0000

    Modificado AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
-            
             new FOS\UserBundle\FOSUserBundle(),
         );
 
